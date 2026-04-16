5f9f76861df0a6d70ea72e0b4dda1fa9f6b24c00
jamalapriadi <jamal.apriadi@gmail.com>
2026-04-16 19:08:40 +0700

    add input component

---
+++ src/components/ui/InputPassword.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+interface InputPasswordProps {
+  label: string;
+  nama: string;
+  error?: string;
+  register: any;
+}
+
+export const InputPassword: React.FC<InputPasswordProps> = ({
+  label,
+  nama,
+  error,
+  register,
+}) => {
+  const [show, setShow] = useState<boolean>(false);
+
+  return (
+    <div className="flex flex-col gap-1 mb-4">
+      <label htmlFor={label}>{label}</label>
+
+      <div className="relative">
+        <input
+          type={show ? "text" : "password"}
+          {...register(nama)}
+          placeholder={label}
+          className="border p-2 w-full px-3 py-2 pr-10"
+        />
+        <button
+          type="button"
+          onClick={() => setShow(!show)}
+          className="absolute right-2 top-2 text-sm"
+        >
+          {show ? "Hide" : "Show"}
+        </button>
+      </div>
+
+      {error && <p className="text-red-500 text-sm">{error}</p>}
+    </div>
+  );
+};
