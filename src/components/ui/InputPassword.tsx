c5419fd9a77c46660d9a19139264f5ccdfebb599
jamalapriadi <jamal.apriadi@gmail.com>
2026-04-24 14:11:46 +0700

    add styling component input

--- src/components/ui/InputPassword.tsx
+++ src/components/ui/InputPassword.tsx
@@ -24,7 +24,13 @@ export const InputPassword: React.FC<InputPasswordProps> = ({
           type={show ? "text" : "password"}
           {...register(nama)}
           placeholder={label}
-          className="border p-2 w-full px-3 py-2 pr-10"
+          className={`border p-2 w-full px-3 py-2 pr-10 rounded-md transition-all outline-none
+            ${
+              error
+                ? "border-red-500 focus:border-red-500 focus:ring-1 focus:ring-red-500 bg-red-50"
+                : "border-gray-300 focus:border-blue-200 focus:ring-2 focus:ring-blue-200 bg-white"
+            }
+          `}
         />
         <button
           type="button"
--- src/components/ui/InputText.tsx
+++ src/components/ui/InputText.tsx
@@ -18,7 +18,13 @@ export const InputText: React.FC<InputTextProps> = ({
         type="text"
         {...register(nama)}
         placeholder={label}
-        className="border p-2"
+        className={`border p-2 rounded-md transition-all outline-none
+          ${
+            error
+              ? "border-red-500 focus:border-red-500 focus:ring-1 focus:ring-red-500 bg-red-50"
+              : "border-gray-300 focus:border-blue-200 focus:ring-2 focus:ring-blue-200 bg-white"
+          }
+        `}
       />
       {error && <p className="text-red-500 text-sm">{error}</p>}
     </div>
